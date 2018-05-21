ea64a0330ec99cd049a41dab22dc1fce98a3bf0b
Ayush Agrawal <ayushthetechie@gmail.com>
2018-05-21 15:41:44 +0530

    Implemented basic forms & validation

---
+++ testapp/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.core import validators
+from testapp.models import Semester
+
+class FormName(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    v_email = forms.EmailField(label = "ENter email again")
+    text = forms.CharField(widget = forms.Textarea)
+#    botcatcher = forms.CharField(required = False, widget= forms.HiddenInput, validators =[validators.MaxLengthValidator(0)])
+
+    def clean(self):
+        cleandata = super().clean()
+        emale = cleandata['email']
+        vemale = cleandata['v_email']
+        if emale != vemale:
+            raise forms.ValidationError("Emails don't match!")
+    # def clean_botcatcher(self):
+    #     botcatcher = self.cleaned_data['botcatcher']
+    #     if len(botcatcher) > 0:
+    #         raise forms.ValidationError("GOTCHA Bot!")
+    #     return botcatcher
+   
--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from testapp.models import Semester,Branch,Subject
+from . import forms
 # Create your views here.
 
 def index(request):
@@ -10,3 +11,17 @@ def index(request):
 
 def about(request):
     return HttpResponse("<h1>This page is under construction</h1></br><h3>Check back later in sometime.</h3>")
+
+def formpg(request):
+    form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print("Validation success")
+            print("Name: "+form.cleaned_data['name'])
+            print("Name: "+form.cleaned_data['email'])
+            print("Name: "+form.cleaned_data['text'])
+                        
+    return render(request,'testapp/forms.html',{'form':form})
