9dc41af1865ddfc2cea2fd9fb1ac32b4d677facc
Ayush Agrawal <ayushthetechie@gmail.com>
2018-05-27 02:19:12 +0530

    Base template added.

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+#Template Tagging
+app_name = 'testapp'
+
 urlpatterns = [
     path('',views.index,name='index'),
     path('about/',views.about,name='about'),
